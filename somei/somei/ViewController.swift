06efa38695ca4cc94edad6eb734e26467087b1ed
Cristiano Correia <cristiano4861@gmail.com>
2019-11-28 10:41:21 -0300

    Projeto Xcode Criado

---
+++ somei/somei/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  somei
+//
+//  Created by Cristiano Correia on 28/11/19.
+//  Copyright © 2019 Cristiano Correia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
