970322576c81f087b5cde1fffeba6dd1741fd2d9
Cristiano Correia <cristiano4861@gmail.com>
2019-12-02 10:02:56 -0300

    crisDevelop Initial Commit

---
+++ somei/somei/Controller/ProcurarProfissionaisViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ProcurarProfissionaisViewController.swift
+//  Somei
+//
+//  Created by Cristiano Correia on 02/12/19.
+//  Copyright © 2019 José Guilherme Bestel. All rights reserved.
+//
+
+import UIKit
+
+class ProcurarProfissionaisViewController: UIViewController {
+    
+    
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+    }
+    
+    func atualizarInterface(){
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ somei/somei/Model/Empresa.swift
@@ -0,0 +1,38 @@
+//
+//  Empresa.swift
+//  somei
+//
+//  Created by Cristiano Correia on 28/11/19.
+//  Copyright © 2019 Cristiano Correia. All rights reserved.
+//
+
+import Foundation
+
+class Empresa {
+    
+    var cnpj : String
+    var nomeFantasia: String
+    var endereco : String
+    var avaliacoes : [Avaliacao]
+    var categoria : String
+    var telefone : String
+    var email : String
+    
+    init (cnpj: String, nomeFantasia: String, endereco: String, avaliacoes: [Avaliacao], categoria: String, telefone: String, email: String){
+     
+        self.cnpj = cnpj
+        self.nomeFantasia = nomeFantasia
+        self.endereco = endereco
+        self.avaliacoes = avaliacoes
+        self.categoria = categoria
+        self.telefone = telefone
+        self.email = email
+    }
+    
+    func avaliar(numNota: Int) {
+        
+        
+        
+    }
+    
+}
