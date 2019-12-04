9dfc6bb30a6b356251ad7b2e1150f41215599b1d
Cristiano Correia <cristiano4861@gmail.com>
2019-12-04 11:33:15 -0300

    Adição view AdicionarComentarioVC
    - função de dismiss do teclado
    - ligar elementos do storyboard na controller

---
+++ somei/somei/Controller/Profissionais/AdicionarComentárioViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AdicionarComentárioViewController.swift
+//  Somei
+//
+//  Created by Cristiano Correia on 04/12/19.
+//  Copyright © 2019 José Guilherme Bestel. All rights reserved.
+//
+
+import UIKit
+
+class AdicionarComenta_rioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ somei/somei/Controller/Profissionais/Cell/ComentarioTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ComentarioTableViewCell.swift
+//  Somei
+//
+//  Created by Cristiano Correia on 04/12/19.
+//  Copyright © 2019 José Guilherme Bestel. All rights reserved.
+//
+
+import UIKit
+
+class ComentarioTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
