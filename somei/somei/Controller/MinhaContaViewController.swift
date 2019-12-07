afbced1cb1e7675a0e28063daeaa648eeba7e2e9
conradodembiski <Jc11286400*>
2019-12-07 16:47:25 -0300

    Varias Fita

---
+++ somei/somei/Controller/MinhaContaViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MinhaContaViewController.swift
+//  Somei
+//
+//  Created by João Conrado Santana de Lima Dembiski on 07/12/19.
+//  Copyright © 2019 José Guilherme Bestel. All rights reserved.
+//
+
+import UIKit
+
+class MinhaContaViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
